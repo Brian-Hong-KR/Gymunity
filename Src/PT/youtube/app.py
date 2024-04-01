5208c61768abf573fc140463111b45e53da830b6
Brian Hong <131517821+Brian-Hong-KR@users.noreply.github.com>
2024-04-01 19:04:06 +0900

    youtube + chatbot merge

--- Src/PT/youtube/app.py
+++ Src/PT/youtube/app.py
@@ -8,5 +8,10 @@ def home():
 
     return render_template("index.html", video_list=video_list)
 
+@app.route("/get", methods=["POST"])
+def chatbot_response():
+    msg = request.form["msg"]    
+    return msg + "qwe"
+
 if __name__ == "__main__":
     app.run(debug=True)
