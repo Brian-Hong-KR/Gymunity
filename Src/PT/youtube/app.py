a4defd30b63a6389cc1eea29af271ed2a1e9bae6
Brian Hong <131517821+Brian-Hong-KR@users.noreply.github.com>
2024-04-01 18:21:48 +0900

    list 순차 재생
    기존 : 재생목록 순차 재생
    변경 : 내가 추가한 동영상 목록 재생하고, 단위마다 API 호출 가능

--- Src/PT/youtube/app.py
+++ Src/PT/youtube/app.py
@@ -1,15 +1,12 @@
-# libraries
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def home():
-    return render_template("index.html", video_list='PLPPetu1spkeZBvp2eLJEAyzr1e6quiJAM')
+    video_list = """YWpKc1R2HBE,rBMgABLzXUQ"""
 
-# playlist?list=PLPPetu1spkeZBvp2eLJEAyzr1e6quiJAM&si=FUlsLj5wdXcNCICW
-
+    return render_template("index.html", video_list=video_list)
 
 if __name__ == "__main__":
     app.run(debug=True)
