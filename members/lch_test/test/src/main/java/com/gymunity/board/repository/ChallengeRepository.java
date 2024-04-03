e6f23ee67c989a577084e24ad1e948bf233dfd58
lch <cogus010307@gmail.com>
2024-04-03 18:21:23 +0900

    회원 + 게시판
    합치고 db 연동 됨(게시물 작성까지만)

---
+++ members/lch_test/test/src/main/java/com/gymunity/board/repository/ChallengeRepository.java
@@ -0,0 +1,27 @@
+package com.gymunity.board.repository;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import com.gymunity.board.dto.ChallengeDTO;
+import com.gymunity.board.dto.PageDTO;
+
+@Mapper
+@Repository
+public interface ChallengeRepository {
+	
+	public int count();
+
+	public List<ChallengeDTO> list(PageDTO pv);
+
+	public ChallengeDTO content(int ch_code);
+
+	public void save(ChallengeDTO dto);
+	
+	public void update(ChallengeDTO dto);
+	
+	public void delete(int ch_code);
+
+}
