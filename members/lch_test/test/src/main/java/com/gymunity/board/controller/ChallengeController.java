3628fdf0f43f630e163091fc127a6813f5ac6bd5
12gina <cometome1212@gmail.com>
2024-04-04 17:24:51 +0900

    operation 어노테이션 추가
    operation 어노테이션 추가

--- members/lch_test/test/src/main/java/com/gymunity/board/controller/ChallengeController.java
+++ members/lch_test/test/src/main/java/com/gymunity/board/controller/ChallengeController.java
@@ -18,6 +18,7 @@ import com.gymunity.board.dto.ChallengeDTO;
 import com.gymunity.board.dto.PageDTO;
 import com.gymunity.board.service.ChallengeService;
 
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
@@ -38,7 +39,7 @@ public class ChallengeController {
 		
 	}
 	
-	
+	@Operation(summary = "챌린지 리스트", description = "챌린지 리스트 API")
     @GetMapping("/challenge/list/{currentPage}")
     public ResponseEntity<Map<String, Object>> listExecute(@PathVariable("currentPage") int currentPage) {
         Map<String, Object> map = new HashMap<>();
@@ -56,6 +57,7 @@ public class ChallengeController {
         return ResponseEntity.ok(map);
     }
 	
+	@Operation(summary = "챌린지 생성", description = "챌린지 생성 API")
 	@PostMapping("/challenge/create")
 	public ResponseEntity<String> writeProExecute(@RequestBody ChallengeDTO dto, PageDTO pv) {
 		log.info("userid:{}, title:{}", dto.getUser_id(), dto.getTitle());
@@ -64,12 +66,14 @@ public class ChallengeController {
 		return ResponseEntity.ok(String.valueOf(1));
 	}
 	
+	@Operation(summary = "챌린지 상세보기", description = "챌린지 상세보기 API")
 	@GetMapping("/challenge/view/{ch_id}")
 	public ResponseEntity<ChallengeDTO> viewExecute(@PathVariable("ch_id") int ch_id) {
 		ChallengeDTO dto = challengeService.contentProcess(ch_id);
 		return ResponseEntity.ok(dto);
 	}
 	
+	@Operation(summary = "챌린지 수정", description = "챌린지 수정 API")
 	@PutMapping("/challenge/update")
 	public ResponseEntity<Object> updateExecute(ChallengeDTO dto, HttpServletRequest req) {
 		log.info("ch_id:{}, title:{}", dto.getCh_id(), dto.getTitle());
@@ -77,6 +81,7 @@ public class ChallengeController {
 		return ResponseEntity.ok(null);
 	}
 
+	@Operation(summary = "챌린지 삭제", description = "챌린지 삭제 API")
 	@DeleteMapping("/challenge/delete/{ch_id}")
 	public ResponseEntity<Object> deleteExecute(@PathVariable("ch_id") int ch_id) {
 		challengeService.deleteProcess(ch_id);
