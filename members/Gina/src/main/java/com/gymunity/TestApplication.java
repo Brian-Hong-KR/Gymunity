8786fb0e6b5f8f3000d3cbb6406fe608544efcab
12gina <cometome1212@gmail.com>
2024-04-05 18:37:06 +0900

    Revert "폴더 복구 후 본인 파일 정리"
    This reverts commit b7b78f28f5fd99516c0323f255ce2b56c4fba198.

--- members/Gina/src/main/java/com/gymunity/TestApplication.java
+++ members/Gina/src/main/java/com/gymunity/TestApplication.java
@@ -3,14 +3,15 @@ package com.gymunity;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 //@ComponentScan(basePackages = {"com.gymunity.*.service"})
 @SpringBootApplication
 @MapperScan("com.gymunity.*.repository")
-public class GymunityApplication {
+public class TestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GymunityApplication.class, args);
+		SpringApplication.run(TestApplication.class, args);
 	}
 
 }
--- members/Gina/src/test/java/com/company/TestApplicationTests.java
+++ members/Gina/src/test/java/com/company/TestApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GymunityApplicationTests {
+class TestApplicationTests {
 
 	@Test
 	void contextLoads() {
