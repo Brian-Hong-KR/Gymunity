cfa5ff21fbfc8c017c51c5c38ecde879ec622b0f
12gina <cometome1212@gmail.com>
2024-04-05 10:52:01 +0900

    service 클래스를 찾지 못하는 오류 해결
    service 클래스를 찾지 못하는 오류 해결

--- members/lch_test/test/src/main/java/com/gymunity/security/config/CustomUserDetailService.java
+++ members/lch_test/test/src/main/java/com/gymunity/security/config/CustomUserDetailService.java
@@ -14,7 +14,6 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-
 public class CustomUserDetailService implements UserDetailsService {
 
 	private final UsersRepository usersRepository;
