b7b78f28f5fd99516c0323f255ce2b56c4fba198
12gina <cometome1212@gmail.com>
2024-04-05 18:36:29 +0900

    폴더 복구 후 본인 파일 정리
    폴더 복구 후 본인 파일 정리

--- members/Gina/gymunity/src/main/java/com/gymunity/GymunityApplication.java
+++ members/Gina/gymunity/src/main/java/com/gymunity/GymunityApplication.java
@@ -3,15 +3,14 @@ package com.gymunity;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 //@ComponentScan(basePackages = {"com.gymunity.*.service"})
 @SpringBootApplication
 @MapperScan("com.gymunity.*.repository")
-public class TestApplication {
+public class GymunityApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TestApplication.class, args);
+		SpringApplication.run(GymunityApplication.class, args);
 	}
 
 }
