c4d5fc67af3146abde1090a1e676b58509ae685e
Brian Hong <hubullage@gmail.com>
2024-04-11 19:56:28 +0900

    Spring Server 추가

--- Release/v0_2_0/Run.sh
+++ Release/v0_2_0/Run.sh
@@ -1,6 +1,13 @@
-systemctl
 
 
+
+sudo systemctl restart mysql
+
+ollama serve
+sudo systemctl restart nginx
+cd Flask
+gunicorn --bind unix:/tmp/myproject.sock "__init__:create_app()"
 
 cd React
 yarn start react
+
