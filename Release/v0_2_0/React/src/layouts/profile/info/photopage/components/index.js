a950d649af4b952ac0e4082dc9a4195fc19f521b
minsaee <kmh930526@gmail.com>
2024-04-19 18:14:06 +0900

    0419 사진첩 리엑트 구현
    DB정보로 리엑트 public에서 찾아서 출력

--- Release/v0_2_0/React/src/layouts/profile/info/photopage/components/index.js
+++ Release/v0_2_0/React/src/layouts/profile/info/photopage/components/index.js
@@ -5,11 +5,57 @@ import axios from "axios";
 
 import SoftBox from "components/SoftBox";
 import SoftTypography from "components/SoftTypography";
-import data from "../data/photoData";
 import Table from "examples/Tables/Table";
 
 const PhotoList = () => {
-  const { columns, rows } = data();
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: localStorage.getItem("Authorization"),
+      "Authorization-refresh": localStorage.getItem("Authorization-refresh"),
+    },
+  };
+
+  const [rows, setRows] = useState([]);
+
+  const columns = [
+    { name: "사진1", align: "center" },
+    { name: "사진2", align: "center" },
+    { name: "챌린지", align: "center" },
+    { name: "날짜", align: "left" },
+  ];
+
+  useEffect(() => {
+    // 이미지 정보를 불러오는 API 호출
+    axios
+      .get(`/photo`, config)
+      .then((response) => {
+        // 받아온 데이터로 rows 상태 업데이트
+        setRows(
+          response.data.map((photo) => ({
+            사진1: (
+              <img
+                src={`${process.env.PUBLIC_URL}/${photo.imagePath1}`}
+                alt="User Photo 1"
+                style={{ width: "50%", height: "auto" }}
+              />
+            ),
+            사진2: photo.imagePath2 ? (
+              <img
+                src={`${process.env.PUBLIC_URL}/${photo.imagePath2}`}
+                alt="User Photo 2"
+                style={{ width: "50%", height: "auto" }}
+              />
+            ) : (
+              <span>No additional photo</span>
+            ),
+            챌린지: photo.challengeTitle,
+            날짜: photo.dateUpdated,
+          }))
+        );
+      })
+      .catch((error) => console.error("Error fetching photos", error));
+  }, []);
 
   return (
     <SoftBox py={1}>
