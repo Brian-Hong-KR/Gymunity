719e39a9a767a6df6bb6f2c3474e0cffeb2bf498
Brian Hong <hubullage@gmail.com>
2024-04-22 17:58:31 +0900

    서비스 퀄리티 & 속도 향상

--- Release/v0_2_0/Flask/Router.py
+++ Release/v0_2_0/Flask/Router.py
@@ -4,40 +4,15 @@ import Chatbot, DataBase
 
 bp = Blueprint('pt_server', __name__, url_prefix='/')
 
-@bp.route('/plan', methods=('POST',))
-def plan():
-    data = request.get_json()
-    gender = data['gender']
-    age = data['age']
-    goal = data['goal']
-    level = data['level']
-    abnormal = data['abnormal']
-
-    plan_name = f"플랜명 : {goal} ({level})"
-    plan_desc = Chatbot.generate_pt_plan( gender=gender, age=age, goal=goal, level=level, abnormal=abnormal)
-
-    response_data = {
-        'plan_name': plan_name,
-        'plan_desc': plan_desc
-    }
-
-    return jsonify(response_data)
-
 @bp.route('/exercise', methods=('POST',))
 def exercise():
     data = request.get_json()
     user_id = data['user_id']
     gender, age, goal, level, abnormal = DataBase.LoadSurveyData(user_id=user_id)
 
-    daily_program = Chatbot.generate_daily_program(gender=gender, age=age, goal=goal, level=level, abnormal=abnormal)
+    videoList = Chatbot.generate_daily_program(gender=gender, age=age, goal=goal, level=level, abnormal=abnormal)
+
     # DataBase.SavePTLog(user_id = user_id, daily_program=str(daily_program))
-
-    videoList = []
-
-    for unit_name in daily_program:
-        videosSearch = VideosSearch("""홈트레이닝 """ + unit_name, limit=1)
-        for video in videosSearch.result()['result']:
-            videoList.append( video['id'] )
 
     response_data = {
         'videoList': videoList
@@ -55,10 +30,6 @@ def chatbot_response():
     user_id = data["user_id"]
     unit_name = data["unit_name"]
     question = data["question"]
-
-    print(user_id)
-    print (unit_name)
-    print(question)
 
     answer = Chatbot.generate_answer(unit_name=unit_name, question=question )
 
@@ -85,19 +56,6 @@ def exercise_done():
 
     return jsonify(response_data)
 
-@bp.route('/store', methods=('POST',))
-def store():
-    user_id = request.form["user_id"]
-
-    gender, age, goal, level, abnormal = DataBase.LoadSurveyData ( user_id )
-
-    product_list = Chatbot.generate_product_list ( gender=gender, age=age, goal=goal, level=level, abnormal=abnormal )
-
-    response_data = {
-        'product_list': product_list,
-    }
-
-    return jsonify(response_data)
 
 
 
