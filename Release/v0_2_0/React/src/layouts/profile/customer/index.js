d6671fbe180ab85fd8ccc25db59588742cc1009c
lch <cogus010307@gmail.com>
2024-04-24 12:34:50 +0900

    고객 문의 수정 완
    고객 문의 수정 완

--- Release/v0_2_0/React/src/layouts/profile/customer/index.js
+++ Release/v0_2_0/React/src/layouts/profile/customer/index.js
@@ -47,12 +47,20 @@ function CustomerCreate(){
         content : "",
      });
 
-    const handleTitleChange = (e) => {
-        setTitle(e.target.value);
+     const handleTitleChange = (e) => {
+        const newTitle = e.target.value;
+        setCustomer(prevState => ({
+            ...prevState,
+            title: newTitle
+        }));
     };
-
+    
     const handleContentChange = (e) => {
-        setContent(e.target.value);
+        const newContent = e.target.value;
+        setCustomer(prevState => ({
+            ...prevState,
+            content: newContent
+        }));
     };
 
     const handleSubmit =  async (e)  => {
