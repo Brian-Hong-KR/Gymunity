06fd1ee9196a492a797857b0d61625d3893ebd01
minsaee <kmh930526@gmail.com>
2024-04-30 17:39:20 +0900

    ㅁㅁ

--- Release/v0_2_0/React/src/layouts/dashboard/info/CsPage.js
+++ Release/v0_2_0/React/src/layouts/dashboard/info/CsPage.js
@@ -7,42 +7,42 @@ import SoftTypography from "components/SoftTypography";
 import Table from "examples/Tables/Table";
 
 const Cs = () => {
-    const baseConfig = {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: localStorage.getItem("Authorization"),
-          "Authorization-refresh": localStorage.getItem("Authorization-refresh"),
-        },
-      };
-    
-      const [rows, setRows] = useState([]);
-    
-      const columns = [
-        { name: "이메일", align: "center" },
-        { name: "제목", align: "center" },
-        { name: "내용", align: "center" },
-        { name: "날짜", align: "center" },
-      ];
-    
-      useEffect(() => {
-        fetchPoints();
-      }, []);
-    
-      const fetchPoints = () => {
-        axios
-          .get(`http://127.0.0.1:8090/user/inquirieslist`, baseConfig)
-          .then((response) => {
-            const cs = response.data.cs;
-            const newRows = cs.map((pointdetail) => ({
-              이메일: pointdetail.userEmail,
-              제목: pointdetail.title,
-              내용: pointdetail.content,
-              날짜: pointdetail.inquiryDate,
-            }));
-            setRows(newRows);
-          })
-          .catch((error) => console.error("Error fetching pointdetail", error));
-      };
+  const baseConfig = {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: localStorage.getItem("Authorization"),
+      "Authorization-refresh": localStorage.getItem("Authorization-refresh"),
+    },
+  };
+
+  const [rows, setRows] = useState([]);
+
+  const columns = [
+    { name: "이메일", align: "center" },
+    { name: "제목", align: "center" },
+    { name: "내용", align: "center" },
+    { name: "날짜", align: "center" },
+  ];
+
+  useEffect(() => {
+    fetchPoints();
+  }, []);
+
+  const fetchPoints = () => {
+    axios
+      .get(`http://127.0.0.1:8090/user/inquirieslist`, baseConfig)
+      .then((response) => {
+        const cs = response.data.cs;
+        const newRows = cs.map((pointdetail) => ({
+          이메일: pointdetail.userEmail,
+          제목: pointdetail.title,
+          내용: pointdetail.content,
+          날짜: pointdetail.inquiryDate,
+        }));
+        setRows(newRows);
+      })
+      .catch((error) => console.error("Error fetching pointdetail", error));
+  };
 
   return (
     <SoftBox py={1}>
@@ -53,7 +53,9 @@ const Cs = () => {
           alignItems="center"
           p={3}
         >
-          <SoftTypography variant="h5">CS 관리</SoftTypography>
+          <SoftTypography variant="h5" alignItems="center">
+            CS 관리
+          </SoftTypography>
         </SoftBox>
 
         <SoftBox
@@ -66,8 +68,8 @@ const Cs = () => {
             },
           }}
         >
-            <div style={{ width: "60%", margin: "0 auto" }}>
-          <Table  columns={columns} rows={rows} />
+          <div style={{ width: "60%", margin: "0 auto" }}>
+            <Table columns={columns} rows={rows} />
           </div>
         </SoftBox>
       </SoftBox>
